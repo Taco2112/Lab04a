d0ee0a739b934d8c1d3e64d5c1c446525b445eff
Sevilla <10488@vtsd.com>
2019-09-24 08:44:07 -0400

    wow

--- Lab04avst.java
+++ Lab04avst.java
@@ -9,10 +9,21 @@ public class Lab04avst
 	{
 		System.out.println("Lab04a, Student Version\n");
 
-		double principal  = 250000;
-		double annualRate = 4.85;
-		double numYears   = 30;
+	//	double principal  = 250000;
+	//	double annualRate = 4.85;
+	//	double numYears   = 30;
 
+
+		double principle = 250000.0;
+		double anual = 0.0485;
+		double numYears = 30;
+
+		double
+
+
+
+		System.out.println(principle);
+		System.out.println(anual * 100);
 
 
 
